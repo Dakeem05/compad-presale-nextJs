999f6ba1c714608ae458e85590e1a7f941675545
Dakeem05 <edidiongsamuel14@gmail.com>
2024-04-19 23:14:53 +0100

    swcMinify change

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
     webpack: (config) => {
       config.resolve.fallback = { fs: false, net: false, tls: false };
       config.externals.push("pino-pretty", "lokijs", "encoding");
+      config.module.rules.forEach((rule) => {
+        if (rule.use && rule.use.loader === 'next-babel-loader') {
+          if (rule.use.options) {
+            rule.use.options.configFile = false;
+            rule.use.options.presets = [['next/babel', { 'swcMinify': false }]];
+          }
+        }
+      });
       return config;
     },
   };
